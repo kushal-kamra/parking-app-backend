1d7b6878805c78a91823f4b97025957b707bd8a2
kushal-kamra <kushalkamradev@gmail.com>
2023-05-09 11:59:31 +0530

    Added vehicle type and vehicle category

---
+++ const/VehicleCategory.js
@@ -0,0 +1,7 @@
+const VehicleCategory = Object.freeze([
+    '2wheeler',
+    '4wheeler-light',
+    '4wheeler-heavy'
+])
+
+export default VehicleCategory;
---
+++ enums/VehicleTypes.js
@@ -0,0 +1,12 @@
+import VehicleCategory from "../const/VehicleCategory";
+
+const VehicleTypes = Object.freeze({
+    motorcycle: VehicleCategory[0],
+    scooter: VehicleCategory[0],
+    car: VehicleCategory[1],
+    suv: VehicleCategory[1],
+    bus: VehicleCategory[2],
+    truck: VehicleCategory[2],
+});
+
+export default VehicleTypes;
