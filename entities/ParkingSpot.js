8a4fd465543585c370ece4e76825e7c4ed06f884
kushal-kamra <kushalkamradev@gmail.com>
2023-05-09 23:20:28 +0530

    Added ParkingSpot.parkVehicle function and test case

--- entities/ParkingSpot.js
+++ entities/ParkingSpot.js
@@ -1,3 +1,5 @@
+import Vehicle from "./Vehicle";
+
 class ParkingSpot {
     constructor(id) {
         if (!Number.isInteger(id)) {
@@ -7,6 +9,14 @@ class ParkingSpot {
         this.id = id;
         this.vehicle = null;
         this.isEmpty = true;
+    }
+
+    parkVehicle(vehicle) {
+        if (!(vehicle instanceof Vehicle)) {
+            throw new Error('Invalid Vehicle Provided');
+        }
+        this.isEmpty = false;
+        this.vehicle = vehicle;
     }
 }
 
--- test/entities/ParkingSpot.test.js
+++ test/entities/ParkingSpot.test.js
@@ -19,12 +19,40 @@ describe("ParkingSpot Class", () => {
     });
 
     it("Check id passed in constructor is valid", async () => {
-        const number = '1234';
-        const newVehicle = new Vehicle(VehicleTypes.motorcycle, number);
         const id = "1";
 
         expect(() => {
-            new ParkingSpot(id, newVehicle);
+            new ParkingSpot(id);
         }).toThrow(new Error('Incorrect ID Provided'));
+    });
+
+    it("Check ParkingSpot.parkVehicle() check isEmpty is false", async () => {
+        const id = 1;
+        const newParkingSpot = new ParkingSpot(id);
+        const number = '1234';
+        const newVehicle = new Vehicle(VehicleTypes.motorcycle, number);
+        newParkingSpot.parkVehicle(newVehicle);
+
+        expect(newParkingSpot).toEqual(
+            expect.objectContaining({
+                id,
+                vehicle: expect.any(Vehicle),
+                isEmpty: false
+            })
+        );
+    });
+
+    it("Check ParkingSpot.parkVehicle() check vehicle is Vehicle object", async () => {
+        const id = 1;
+        const newParkingSpot = new ParkingSpot(id);
+        const number = '1234';
+        const newVehicle = {
+            type: VehicleTypes.motorcycle,
+            number
+        };
+        
+        expect(() => {
+            newParkingSpot.parkVehicle(newVehicle);
+        }).toThrow(new Error('Invalid Vehicle Provided'));
     });
 });
