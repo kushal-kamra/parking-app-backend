63b2cf0bd1751a35156b026c5e9962d571097b61
kushal-kamra <kushalkamradev@gmail.com>
2023-05-09 22:42:37 +0530

    Added Parking Spot class and test cases

--- entities/ParkingSpot.js
+++ entities/ParkingSpot.js
@@ -1,5 +1,13 @@
+import Vehicle from "./Vehicle";
+
 class ParkingSpot {
     constructor(id, vehicle) {
+        if (!Number.isInteger(id)) {
+            throw new Error('Incorrect ID Provided');
+        }
+        if (!(vehicle instanceof Vehicle)) {
+            throw new Error('Incorrect Vehicle Object Provided');
+        }
         this.id = id;
         this.vehicle = vehicle;
         this.isEmpty = true;
--- test/entities/ParkingSpot.test.js
+++ test/entities/ParkingSpot.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-new */
 /* eslint-disable no-undef */
 import ParkingSpot from "../../entities/ParkingSpot";
 import Vehicle from "../../entities/Vehicle";
@@ -17,5 +18,28 @@ describe("ParkingSpot Class", () => {
                 vehicle: expect.any(Vehicle),
             })
         );
+    });
+
+    it("Check id passed in constructor is valid", async () => {
+        const number = '1234';
+        const newVehicle = new Vehicle(VehicleTypes.motorcycle, number);
+        const id = "1";
+
+        expect(() => {
+            new ParkingSpot(id, newVehicle);
+        }).toThrow(new Error('Incorrect ID Provided'));
+    });
+
+    it("Check vehicle passed in constructor is valid", async () => {
+        const number = '1234';
+        const newVehicle = {
+            type: VehicleTypes.motorcycle,
+            number
+        };
+        const id = 1;
+
+        expect(() => {
+            new ParkingSpot(id, newVehicle);
+        }).toThrow(new Error('Incorrect Vehicle Object Provided'));
     });
 });
