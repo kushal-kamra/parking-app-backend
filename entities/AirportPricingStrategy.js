47caa74bfa52b2d50b1a32f7fd166c79fa507ab5
kushal-kamra <kushalkamradev@gmail.com>
2023-05-26 09:02:26 +0530

    Added airport strategy

---
+++ entities/AirportPricingStrategy.js
@@ -0,0 +1,46 @@
+/* eslint-disable eqeqeq */
+/* eslint-disable no-else-return */
+import PricingStrategy from "./PricingStrategy";
+import VehicleCategory from "../const/VehicleCategory";
+
+class AirportPricingStrategy extends PricingStrategy {
+    // eslint-disable-next-line class-methods-use-this
+    calculateFees(entryTimestamp, vehicleCategory) {
+        if (!Number.isInteger(entryTimestamp)) {
+            throw new Error('Incorrect entryTimestamp Provided');
+        }
+        
+        if (!VehicleCategory.includes(vehicleCategory)) {
+            throw new Error('Incorrect vehicleCategory Provided');
+        }
+
+        const hoursPassed = Math.abs(entryTimestamp - Date.now()) / 3600000;
+        let fees = 0;
+
+        if (vehicleCategory == VehicleCategory[0]) {
+            if (hoursPassed >= 1) {
+                fees += 40;
+            }
+            if (hoursPassed >= 8) {
+                fees += 60;
+            }
+            if (hoursPassed >= 24) {
+                fees += (Math.ceil((Math.ceil(hoursPassed) - 24)/24) * 80);
+            }
+        } else if (vehicleCategory == VehicleCategory[1]) {
+            if (hoursPassed > 0) {
+                fees += 60;
+            }
+            if (hoursPassed >= 12) {
+                fees += 80;
+            }
+            if (hoursPassed >= 24) {
+                fees += (Math.ceil((Math.ceil(hoursPassed) - 24)/24) * 100);;
+            }
+        }
+
+        return fees;
+    }
+}
+
+export default AirportPricingStrategy;
---
+++ test/entities/AirportPricingStrategy.test.js
@@ -0,0 +1,79 @@
+/* eslint-disable no-undef */
+import VehicleCategory from "../../const/VehicleCategory";
+import AirportPricingStrategy from "../../entities/AirportPricingStrategy";
+
+describe("AirportPricingStrategy Class", () => {
+    it("Check AirportPricingStrategy for 2wheeler + 30 mins", async () => {
+        const AirportPricing = new AirportPricingStrategy();
+        const date3HoursBefore = Date.now() - 1800000;
+        const fees = AirportPricing.calculateFees(date3HoursBefore, VehicleCategory[0]);
+
+        expect(fees).toEqual(0);
+    });
+    
+    it("Check AirportPricingStrategy for 2wheeler + 5 hours", async () => {
+        const AirportPricing = new AirportPricingStrategy();
+        const date5HoursBefore = Date.now() - 18000000;
+        const fees = AirportPricing.calculateFees(date5HoursBefore, VehicleCategory[0]);
+
+        expect(fees).toEqual(40);
+    });
+
+    it("Check AirportPricingStrategy for 2wheeler + 50 hours", async () => {
+        const AirportPricing = new AirportPricingStrategy();
+        const date50HoursBefore = Date.now() - 180000000;
+        const fees = AirportPricing.calculateFees(date50HoursBefore, VehicleCategory[0]);
+
+        expect(fees).toEqual(260);
+    });
+
+    it("Check AirportPricingStrategy for 4wheeler-light + 30 mins", async () => {
+        const AirportPricing = new AirportPricingStrategy();
+        const date3HoursBefore = Date.now() - 1800000;
+        const fees = AirportPricing.calculateFees(date3HoursBefore, VehicleCategory[1]);
+
+        expect(fees).toEqual(60);
+    });
+    
+    it("Check AirportPricingStrategy for 4wheeler-light + 5 hours", async () => {
+        const AirportPricing = new AirportPricingStrategy();
+        const date5HoursBefore = Date.now() - 18000000;
+        const fees = AirportPricing.calculateFees(date5HoursBefore, VehicleCategory[1]);
+
+        expect(fees).toEqual(60);
+    });
+
+    it("Check AirportPricingStrategy for 4wheeler-light + 50 hours", async () => {
+        const AirportPricing = new AirportPricingStrategy();
+        const date50HoursBefore = Date.now() - 180000000;
+        const fees = AirportPricing.calculateFees(date50HoursBefore, VehicleCategory[1]);
+
+        expect(fees).toEqual(340);
+    });
+
+    it("Check AirportPricingStrategy for 4wheeler-heavy", async () => {
+        const AirportPricing = new AirportPricingStrategy();
+        const date5HoursBefore = Date.now() - 18000000;
+        const fees = AirportPricing.calculateFees(date5HoursBefore, VehicleCategory[2]);
+
+        expect(fees).toEqual(0);
+    });
+
+    it("Check AirportPricingStrategy passed in calculateFees is valid", async () => {
+        expect(() => {
+            // eslint-disable-next-line no-new
+            const AirportPricing = new AirportPricingStrategy();
+            const date5HoursBefore = Date.now() - 18000000;
+            AirportPricing.calculateFees(date5HoursBefore, "2wheelers");
+        }).toThrow(new Error('Incorrect vehicleCategory Provided'));
+    });
+
+    it("Check entryTimestamp passed in calculateFees is valid", async () => {
+        expect(() => {
+            // eslint-disable-next-line no-new
+            const AirportPricing = new AirportPricingStrategy();
+            const date5HoursBefore = `${Date.now() - 18000000}`;
+            AirportPricing.calculateFees(date5HoursBefore, VehicleCategory[0]);
+        }).toThrow(new Error('Incorrect entryTimestamp Provided'));
+    });
+})
