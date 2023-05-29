a7a9e11a212eebbf999de61fa855f7578c55252d
kushal-kamra <kushalkamradev@gmail.com>
2023-05-29 16:58:08 +0530

    renaming ex

--- example1.js
+++ example1.js
@@ -4,8 +4,8 @@ import VehicleTypes from "./enums/VehicleTypes.js";
 
 const mall = new ParkingSpotManager(2, 0, 0, PricingStrategies[0]);
 
-const vehicle1 = mall.parkVehicle(VehicleTypes.motorcycle, '1234', Date.now());
-const vehicle2 = mall.parkVehicle(VehicleTypes.scooter, '5678', Date.now());
+const motorcycle = mall.parkVehicle(VehicleTypes.motorcycle, '1234', Date.now());
+const scooter = mall.parkVehicle(VehicleTypes.scooter, '5678', Date.now());
 mall.parkVehicle(VehicleTypes.scooter, '1357', Date.now());
-mall.unparkVehicle(VehicleTypes.scooter, vehicle2, Date.now() + 2400000);
-mall.unparkVehicle(VehicleTypes.motorcycle, vehicle1, Date.now() + 13200000);
+mall.unparkVehicle(VehicleTypes.scooter, scooter, Date.now() + 2400000);
+mall.unparkVehicle(VehicleTypes.motorcycle, motorcycle, Date.now() + 13200000);
